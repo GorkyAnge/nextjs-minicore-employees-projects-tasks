7badce99a3cc391bc6d8449ed109247645e6f2b3
Gorky Palacios <gorky.palacios@paymentevolution.com>
2024-10-16 16:37:28 -0500

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+'use client'
+import { useEffect, useState } from "react";
+
+
+export default function Home() {
+ 
+
+
+  return (
+    <main className="px-4 sm:px-6 lg:px-8">
+     
+    </main>
+  );
+}
